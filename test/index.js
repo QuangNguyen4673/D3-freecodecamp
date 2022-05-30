b0516018852283026c4dc5390ffbfe568b4f942c
Quang <69511888+QuangNguyen4673@users.noreply.github.com>
2022-05-30 17:33:47 +0700

    doing research page 130

--- test/index.js
+++ test/index.js
@@ -1,3 +1,59 @@
-d3.csv("test.csv", function (error, data) {
-  console.log(error, data)
-})
+const dataset = [
+  [480, 90],
+  [5, 20],
+  [250, 50],
+  [100, 33],
+  [330, 95],
+  [410, 12],
+  [475, 44],
+  [25, 67],
+  [85, 21],
+  [220, 88],
+]
+let w = 800
+let h = 200
+let padding = 50
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h)
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[0])])
+  .range([padding, w - padding])
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[1])])
+  .range([h - padding, padding])
+
+const aScale = d3
+  .scaleSqrt()
+  .domain([0, d3.max(dataset, (d) => d[1])])
+  .range([2, 8])
+svg
+  .selectAll("circle")
+  .data(dataset)
+  .enter()
+  .append("circle")
+  .attr("cx", (d) => xScale(d[0]))
+  .attr("cy", (d) => yScale(d[1]))
+  .attr("r", (d) => {
+    return aScale(d[1])
+  })
+
+svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text((d) => d[0] + "," + d[1])
+  .attr("x", (d) => xScale(d[0]))
+  .attr("y", (d) => yScale(d[1]))
+  .attr("font-size", "20px")
+  .style("fill", "red")
+
+const fooScale = d3
+  .scaleLinear()
+  .domain([0, 500])
+  .range([10, 10000])
+  .clamp(true)
+console.log(fooScale(600))
